34489e3404b7f4dc1649ecc7e1fc9a87794b3722
Francis Mitra <francismitra@gmail.com>
2014-12-19 19:09:21 -0500

    move the init function out of the prototype

--- assets/scripts/game_of_life.js
+++ assets/scripts/game_of_life.js
@@ -1,7 +1,3 @@
-// cell [x, y]
-// live [[cell][cell][cell]]
-
-// 9x9 grid
 var example_cells = [
   [0,0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0,0],
@@ -14,6 +10,12 @@ var example_cells = [
   [0,0,0,0,0,0,0,0,0]
 ];
 
+function Cell(x_position, y_position, state) {
+    this.x_position = x_position;
+    this.y_position = y_position;
+    this.state      = state;
+}
+
 function Life(cell_array) {
     // Raw data values for processing
     this.cell_array = cell_array;
@@ -21,23 +23,15 @@ function Life(cell_array) {
     this.current_generation = [];
     // The pending creation of Life objects
     this.next_generation = [];
-}
 
-function Cell(x_position, y_position, state) {
-    this.x_position = x_position;
-    this.y_position = y_position;
-    this.state      = state;
-}
+    /*--------------------------------------------
 
+      On intialization, we parse a multi-dimensional array
+      of values and instantiate them as Cell objects, and likewise
+      create a Collection of Cells
 
-Life.prototype = {
-
-    // On intialization, we parse a multi-dimensional array
-    // of values and instantiate them as Cell objects, and likewise
-    // create a Collection of Cells
-    initialize: function() {
-
-        var self = this;
+    --------------------------------------------*/
+    function _initialize() {
 
         // Vertical loop
         for (var y = 0; y < this.cell_array.length; y++) {
@@ -51,7 +45,9 @@ Life.prototype = {
                   in state and new_cell. We are determining the 
                   exact position first according to the loop [y][x]
                   and then constructing a new cell with the coordinates
-                  in order [x, y]
+                  in order [x, y]... not necessary but it helps
+                  me visiualize the grid easier when 'x' 
+                  is noted before 'y'
                     
                   Given a grid of 9 arrays with 9 values, it will
                   produce 81 (9x9) cells
@@ -66,7 +62,13 @@ Life.prototype = {
 
         }
 
-    },
+    };
+
+    _initialize.apply(this);
+}
+
+
+Life.prototype = {
 
     determineNewCellState: function() {
 
@@ -77,8 +79,6 @@ Life.prototype = {
 
         for (var i = 0; i < this.current_generation.length; i++) {
 
-            // console.log(this.current_generation[i]);
-
             var cell = this.current_generation[i];
 
             var row_above = (cell.y_position-1 >= 0) ? cell.y_position-1 : 9-1;
@@ -86,6 +86,7 @@ Life.prototype = {
             var column_left = (cell.x_position-1 >= 0) ? cell.x_position-1 : 9-1;
             var column_right = (cell.x_position+1 <= 9-1) ? cell.x_position+1 : 0;
 
+            // Build a collection of neighboring Cell objects
             var neighbors = {
 
                 top_left: _.findWhere(this.current_generation, {'x_position': column_left, 'y_position': row_above}),
@@ -106,6 +107,8 @@ Life.prototype = {
                 (neighbors[n].state == 'alive') ? alive_count++ : dead_count++;
             }
 
+            // Determine pending cell state based on Conway's rules
+            // http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
             var state = cell.state;
             if (cell.state == 'alive') {
                 if(alive_count < 2 || alive_count > 3) {
@@ -149,10 +152,10 @@ Life.prototype = {
         canvas.strokeStyle = '#e1e1e1';
         canvas.fillStyle = '#000000';
 
-        var start_x = cell.getPositionX() * size;
-        var start_y = cell.getPositionY() * size;
+        var start_x = cell.x_position * size;
+        var start_y = cell.y_position * size;
 
-        if (cell.getState() == 'alive') {
+        if (cell.state == 'alive') {
             canvas.fillRect(start_x, start_y, size, size);
         } else {
             canvas.clearRect(start_x, start_y, size, size);
@@ -162,24 +165,6 @@ Life.prototype = {
 
 }
 
-Cell.prototype = {
-
-    getPositionX: function() {
-        return this.x_position;
-    },
-    getPositionY: function() {
-        return this.y_position;
-    },
-    getState: function() {
-        return this.state;
-    },
-    setState: function(state) {
-        this.state = state;
-    },
-
-}
 
 var life = new Life(example_cells);
-
-life.initialize();
 life.determineNewCellState();
